14a803636e75cbbbae567016ce67b2c4d6536dba
Paros Rangsha <paroscrangsha@gmail.com>
2024-11-12 05:19:06 +0000

    script delte

--- src/assets/components/question/Question.jsx
+++ src/assets/components/question/Question.jsx
@@ -5,19 +5,6 @@ import Quesreuseable from '../reuseable/quesreuseable'
 import './script.js'
 
 function Question() {
-    let btnHidShow = document.querySelectorAll(".hideshow")
-    let arrayBtnHidShow = Array.from(btnHidShow)
-    arrayBtnHidShow.map((item, index)=>{
-        item.addEventListener("click",()=>{                
-            let para = document.querySelectorAll(".mypara")
-            let arrayPara = Array.from(para)
-            if(arrayPara[index].style.display = "none"){
-                arrayPara[index].style.display = "block"
-            }else{
-                arrayPara[index].style.display = "none"
-            }
-        })
-    })
   return (
     <div className='question'>
         <Container>
